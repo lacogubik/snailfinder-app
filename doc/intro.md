9fc128cfca855c450a57a38236d70ad18974c01e
Ladislav Gubik <lacogubik@gmail.com>
2016-06-17 22:20:49 +0100

    Init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to snailfinder-app
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/snailfinder_app/subs.cljs
@@ -0,0 +1,9 @@
+(ns snailfinder-app.subs
+  (:require-macros [reagent.ratom :refer [reaction]])
+  (:require [re-frame.core :refer [register-sub]]))
+
+(register-sub
+  :get-greeting
+  (fn [db _]
+    (reaction
+      (get @db :greeting))))
