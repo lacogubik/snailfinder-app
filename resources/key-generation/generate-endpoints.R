cf825adcd516b9582327ecce3c1d43ff399bdf9d
Thomas in't Veld <thomas.intveld@gmail.com>
2016-06-18 19:40:44 +0100

    endpoint generation for resources

---
+++ resources/key-generation/generate-endpoints.R
@@ -0,0 +1,52 @@
+rm(list=ls())
+df <- read.csv('endpoints.csv', stringsAsFactors = FALSE)
+
+# first some munging to get format of each column exactly right!
+df$node.ID <- tolower(df$node.ID)
+df$node.ID <- paste(':', df$node.ID, sep='')
+
+# :s1  {:name                      ""
+#       :common-name               ""
+#       :family                    ""
+#       :size                      ""
+#       :image-white               ""
+#       :image-shell               ""
+#       :image-habitat             ""
+#       :identification-characters ""
+#       :similar-species           ""
+#       :ecology                   ""
+#       :gb-distribution           ""
+#       :world-distribution        ""
+#       :conservation-status       ""
+#       :map-image                 ""
+#       :notes                     ""}
+
+printEndpoint <- function(xr){
+  if(is.na(xr$notes)){xr$notes<-''}
+  if(is.na(xr$image.1..animals.on.white.)){xr$image<-''}
+  if(is.na(xr$map.image)){xr$map.image<-''}
+  string <- paste(
+    xr$node.ID, ' { ',
+    ':name "', xr$name, '" ',
+    ':common-name "', xr$common.name, '" ',
+    ':family "', xr$family, '" ',
+    ':size "', xr$size, '" ',
+    ':image-white "', xr$image.1..animals.on.white., '" ',
+    ':image-shell "', xr$image.2..shells., '" ',
+    ':image-habitat "', xr$image.3..animals.in.habitat., '" ',
+    ':identification-characters "', xr$identification.characters, '" ',
+    ':similar-species "', xr$similar.species, '" ',
+    ':ecology "', xr$ecology, '" ',
+    ':gb-distribution "', xr$distribution.in.the.British.Isles, '" ',
+    ':world-distribution "', xr$distribution.elsewhere, '" ',
+    ':conservation-status "', xr$conservation.status, '" ',
+    ':map-image "', xr$map.image, '" ',
+    ':notes "', xr$notes, '"}', sep=''
+  )
+  return(string)
+}
+
+endpointsStrings <- sapply(1:nrow(df), function(i) return(printEndpoint(df[i,])))
+
+resultFrame <- c('(def snails {', endpointsStrings, '})')
+write(resultFrame, file='snails-out.txt')
