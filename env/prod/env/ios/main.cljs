9371c06d17cea9805f7168f8725e4cf182361690
Federico Roffi <fedoverseas@gmail.com>
2016-09-20 00:38:08 +0100

    Upgrade re-natal project

---
+++ env/prod/env/ios/main.cljs
@@ -0,0 +1,6 @@
+ (ns env.ios.main
+  (:require [snailfinder-app.ios.core :as core]))
+
+ (core/init)
+
+
---
+++ src/snailfinder_app/db.cljs
@@ -0,0 +1,10 @@
+(ns snailfinder-app.db
+  (:require [clojure.spec :as s]))
+
+;; spec of app-db
+(s/def ::greeting string?)
+(s/def ::app-db
+  (s/keys :req-un [::greeting]))
+
+;; initial state of app-db
+(def app-db {:greeting "Hello Clojure in iOS and Android!"})
